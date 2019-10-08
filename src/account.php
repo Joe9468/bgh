74b75df8615f067a19bb2aafbd2c1b112f980399
EricSimith <ericsimith@163.com>
2019-10-09 01:53:33 +0800

    更改登录页面

--- src/account.php
+++ src/account.php
@@ -1,16 +1,12 @@
 <?php
 
-    include_once 'mysql.php';
+include_once 'sql_connect.php';
 
-    $sql = "SELECT * FROM `tb_users` WHERE `username` LIKE '" . $_GET['username'] . "'";
+$sql = "SELECT * FROM `tb_users` WHERE `username` LIKE '" . $_POST['username'] . "'";
+ 
+$user = $db1->query($sql)->fetch(PDO::FETCH_ASSOC);
 
-    $user = mysql_fetch_assoc(my_query($sql));
-    if (!$user) {
-        echo '用户不存在，请重新输入';  
-    }
-    echo '欢迎您：'.$user['username'];
-    
+session_start();
+$_SESSION = $user;
 
-
-
-
+echo '欢迎您：' . $user['username'];
--- src/sql_connect.php
+++ src/sql_connect.php
@@ -6,7 +6,7 @@ header("Content-type:text/html;charset=utf-8");
 $db1_host = '127.0.0.1';
 $db1_user = 'bgh';
 $db1_password = 'bgh';
-$db1_dbname = 'bgh';
+$db1_dbname = 'db_bgh';
 
 // 数据库二：用来读取财务系统数据
 $db2_host = '127.0.0.1';
@@ -16,14 +16,12 @@ $db2_dbname_kjcw = '';
 $db2_dbname_ys = '';
 
 
-try {
-    $db1 = new PDO("mysql:host=". $db1_host .";dbname=". $db1_dbname,$db1_user,$db1_password);
-    $db1->query("set names utf8");
-    // $db2_ys = new PDO("mysql:host=". $db1_host .";dbname=". $db1_dbname,$db1_user,$db1_password);
-    // $db2_ys->query("set names utf8");
-    // $db2_kj = new PDO("mysql:host=". $db1_host .";dbname=". $db1_dbname,$db1_user,$db1_password);
-    // $db2_kj->query("set names utf8");
-} catch (PDOException $th) {
-    print "ERROR!:" . $th->getMessage() . "<br/>";
-    die();
-}
+// 连接数据库
+$db1 = new PDO("mysql:host=" . $db1_host . ";dbname=" . $db1_dbname, $db1_user, $db1_password, array(PDO::ATTR_PERSISTENT => true));
+// $db2_ys = new PDO("mysql:host=". $db1_host .";dbname=". $db1_dbname,$db1_user,$db1_password, array(PDO::ATTR_PERSISTENT => true));
+// $db2_kj = new PDO("mysql:host=". $db1_host .";dbname=". $db1_dbname,$db1_user,$db1_password, array(PDO::ATTR_PERSISTENT => true));
+
+
+$db1->query("set names utf8");
+// $db2_ys->query("set names utf8");
+// $db2_kj->query("set names utf8");
