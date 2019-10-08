c84f2230350889822f0291cf1b9d3b722251c511
EricSimith <ericsimith@163.com>
2019-10-09 00:48:43 +0800

    更换数据库连接方式为PDO

---
+++ src/sql_connect.php
@@ -0,0 +1,29 @@
+<?php
+
+header("Content-type:text/html;charset=utf-8");
+
+// 数据库一：用来保存办公会相关数据
+$db1_host = '127.0.0.1';
+$db1_user = 'bgh';
+$db1_password = 'bgh';
+$db1_dbname = 'bgh';
+
+// 数据库二：用来读取财务系统数据
+$db2_host = '127.0.0.1';
+$db2_user = 'jcgc';
+$db2_password = '';
+$db2_dbname_kjcw = '';
+$db2_dbname_ys = '';
+
+
+try {
+    $db1 = new PDO("mysql:host=". $db1_host .";dbname=". $db1_dbname,$db1_user,$db1_password);
+    $db1->query("set names utf8");
+    // $db2_ys = new PDO("mysql:host=". $db1_host .";dbname=". $db1_dbname,$db1_user,$db1_password);
+    // $db2_ys->query("set names utf8");
+    // $db2_kj = new PDO("mysql:host=". $db1_host .";dbname=". $db1_dbname,$db1_user,$db1_password);
+    // $db2_kj->query("set names utf8");
+} catch (PDOException $th) {
+    print "ERROR!:" . $th->getMessage() . "<br/>";
+    die();
+}
