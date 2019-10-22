f9ec925d5e595eaa5698ec6b17a7c5169263bf35
EricSimith <ericsimith@163.com>
2019-10-22 16:59:01 +0800

    油料60%

--- js/yltj.js
+++ js/yltj.js
@@ -20,7 +20,6 @@ $("#jy_type ul li a").click(function () {
 // 加油卡号下拉菜单选择
 $("#jy_card ul li a").click(function () {
     $("#jy_card input").val($(this).text())
-
 })
 // 添加数据到车辆牌照下拉菜单
 function init_car_card(cc_arr) {
@@ -42,18 +41,124 @@ function init_jy_card(jc_arr) {
         $("#jy_card").find("ul").append(li_html)
     })
 }
-// 更新月数下拉菜单
-function init_month(now_month) {
-    $("yl_month button").text(now_month[0] + "年" + now_month[1] + "月")
-    now_month[1] += 1
-    for (let i = 0; i < 5; i++) {
-        now_month[1] -= 1
-        yl_month_li += "<li><a href=\"#\">" + now_month + "</a></li>"
-    }
-    $("yl_month ul").append(yl_month_li)
+
+// Date组函数
+function str_to_date(date_str) {
+    date = date_str.split('-')
+    date[0] = parseInt(date[0])
+    date[1] = parseInt(date[1])
+    return date
 }
+
+function addMonth(date, num) {
+    num = parseInt(num)
+    var eYear = date[0]
+    var eMonth = date[1] + num
+    while (eMonth > 12) {
+        eYear++
+        eMonth -= 12
+    }
+    while (eMonth < 1) {
+        eYear--
+        eMonth += 12
+    }
+    date[0] = eYear
+    date[1] = eMonth
+    return date
+}
+
+function date_format(date) {
+    return date[0] + "年" + date[1] + "月"
+}
+
+function format_str(str) {
+    return str.substring(0, 3) + '-' + str.substring(5, 6) + "-01 00:00:00"
+}
+// 初始化月数下拉菜单
+function init_month(date_string) {
+    date = str_to_date(date_string)
+    addMonth(date, 1)
+    action = "yl_list"
+    states = 2
+    $.ajax({
+        type: "get",
+        url: "/src/yltj.php",
+        data: {
+            'action': action,
+            'states': states,
+            'month': format_str(date)
+        },
+        success: function (request) {
+            request = JSON.parse(request)
+            data = JSON.parse(request['data'])
+            $("yl_month button").text(date_format(date))
+            addMonth(date, 1)
+            for (let i = 0; i < 5; i++) {
+                addMonth(date, -1)
+                yl_month_li += "<li><a href=\"#\">" + date_format(date) + "</a></li>"
+            }
+            $("yl_month ul").append(yl_month_li)
+            init_yl_list(data['yl_list'])
+        }
+    })
+}
+// 月数下拉菜单被点击
+$("yl_month ul li a").click(function () {
+    action = "yl_list"
+    states = 2
+    $.ajax({
+        type: "get",
+        url: "/src/yltj.php",
+        data: {
+            'action': action,
+            'states': states,
+            'month': format_str($(this).text())
+        },
+        success: function (request) {
+            request = JSON.parse(request)
+            data = JSON.parse(request['data'])
+            $("yl_month button").text($(this).text())
+            init_yl_list(data['yl_list'])
+        }
+    })
+})
 // 更新油料情况列表
-function init_yl_list() {
+function init_yl_list(yl_list) {
+    $("#yl_list thead tbody").empty()
+    var dw_name = ""
+    var c_je = 0
+    var swyl = 0
+    $.each(yl_list, function (indexInArray, valueOfElement) {
+        tbody_html = "<tr>"
+        tbody_html += "<td style=\"text-align: center;\" class=\"hidden\">" + valueOfElement['ylid'] + "</td>"
+        tbody_html += "<td style=\"text-align: center;\">" + valueOfElement['dw_name'] + "</td>"
+        tbody_html += "<td style=\"text-align: center;\">" + valueOfElement['car_card'] + "</td>"
+        tbody_html += "<td style=\"text-align: center;\">" + valueOfElement['times'] + "</td>"
+        tbody_html += "<td style=\"text-align: center;\">" + valueOfElement['type'] + "</td>"
+        tbody_html += "<td style=\"text-align: center;\">" + valueOfElement['jy_card'] + "</td>"
+        tbody_html += "<td style=\"text-align: center;\">" + valueOfElement['c_je'] + "</td>"
+        tbody_html += "<td style=\"text-align: center;\">" + valueOfElement['swyl'] + "</td>"
+        if (valueOfElement['rz'] == 0) {
+            tbody_html += "<td style=\"text-align: center;\"><button type=\"button\" class=\"btn btn-xs btn-danger\">删除</button></td>"
+        } else {
+            tbody_html += "<td style=\"text-align: center;\"><button type=\"button\" class=\"btn btn-xs btn-danger\" disable>删除</button></td>"
+        }
+        tbody_html += "</tr>"
+        c_je += parseFloat(valueOfElement['c_je'])
+        swyl += parseFloat(valueOfElement['swyl'])
+        if (valueOfElement['dw_name'] != dw_name && dw_name != "") {
+            tbody_html += "<tr>"
+            tbody_html += "<td style=\"text-align: center;\"  colspan = \"6\">"+ valueOfElement['dw_name'] +"汇总</td>"
+            tbody_html += "<td style=\"text-align: center;\">" + c_je + "</td>"
+            tbody_html += "<td style=\"text-align: center;\">" + swyl + "</td>"
+            tbody_html += "<td style=\"text-align: center;\"></td>"
+            tbody_html += "</tr>"
+            c_je = 0
+            swyl = 0
+        }
+        dw_name = valueOfElement['dw_name']
+        $("#yl_list thead tbody").append(tbody_html)
+    })
 
 }
 // 删除数据
@@ -62,7 +167,7 @@ $("#yl_list tbody tr td button").click(function () {
     states = 1
     $.ajax({
         type: "get",
-        url: "../src/yltj.php",
+        url: "src/yltj.php",
         data: {
             'action': action,
             'states': states,
@@ -79,16 +184,17 @@ $(document).ready(function () {
     states = 0
     $.ajax({
         type: "get",
-        url: "../src/yltj.php",
+        url: "src/yltj.php",
         data: {
             'action': action,
             'states': states
         },
         success: function (request) {
-            init_month(request["month"])
-            init_car_card(request["init_cc"])
-            init_jy_card(request["init_jc"])
-            init_yl_list(request["yl_search"])
+            request = JSON.parse(request)
+            data = JSON.parse(request['data'])
+            init_month(data['month'])
+            init_car_card(data['cc_arr'])
+            init_jy_card(data['jc_arr'])
         }
     })
 })
--- src/yltj.php
+++ src/yltj.php
@@ -7,34 +7,52 @@ session_start();
 
 $action = null;
 $states = null;
-$data = null;
+$data = array();
 
-switch ($_GET["states"]) {
-    case 0:
-        break;
-    case 1:
-        // 删除数据
-        $sql = "DELETE FROM `tb_yltj` WHERE `ylid` = " . $_GET['ylid'];
-        $qu = $db1->query($sql);
-        $qu - closeCursor();
-        $action = "del success";
-        $states = 01;
-        break;
-    case 2:
-        break;
+function init_month($db1_f)
+{
+    $sql = "SELECT DISTINCT `month` FROM `tb_yltj` WHERE `rz` = 1 AND `dwid` LIKE '" . $_SESSION['dwid'] . "%' ORDER BY `month` DESC LIMIT 1";
+    $qu = $db1_f->query($sql);
+    $month = $qu->fetch(PDO::FETCH_ASSOC);
+    $qu->closeCursor();
+    return $month;
 }
 
-switch ($_POST["states"]) {
-    case 5:
-        # code...
-        break;
-    case 6:
-        # code...
-        break;
+if ($_POST == null) {
+    switch ($_GET["states"]) {
+        case 0:
+            $action = "init_success";
+            $states = 00;
+            $data = init_month($db1);
+            break;
+        case 1:
+            // 删除数据
+            $sql = "DELETE FROM `tb_yltj` WHERE `ylid` = '" . $_GET['ylid'] . "'";
+            $qu = $db1->query($sql);
+            $qu - closeCursor();
+            $action = "del success";
+            $states = 01;
+            break;
+        case 2:
+            $sql =  "SELECT  'tb_yltj'.*, 'tb_dwxx'.'dw_name' FROM `tb_yltj`,'tb_dwxx' WHERE `dwid` LIKE '" . $_SESSION['dwid'] . "%' ORDER BY `dwid`";
+            break;
+    }
+} else {
+    switch ($_POST["states"]) {
+        case 5:
+            # code...
+            break;
+        case 6:
+            # code...
+            break;
+    }
 }
 
-echo json_encode(array(
+
+$send = json_encode(array(
     'action' => $action,
     'states' => $states,
-    'data' => json_encode($_data)
+    'data' => json_encode($data)
 ));
+
+echo $send;
